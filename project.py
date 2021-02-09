fa1963dd8145da8854849becd5c8c3e26dbbb764
nishant-sg <nishantgarg.sg@gmail.com>
2021-02-09 18:10:04 +0530

    'checkpoint-1'

--- project.py
+++ project.py
@@ -1,7 +1,8 @@
 import cv2 
 import numpy
 
-img = cv2.imread('CHECKPOINT1.png')
+
+img = cv2.imread('c3.png')
 
 height, width, channel = img.shape
 img = cv2.resize(img , (0,0), fx=1 , fy=1)
@@ -16,11 +17,13 @@ contours, hierarchy = cv2.findContours(imgc, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX
 
 for c in contours:
 	M = cv2.moments(c)
-	cx = int(M['m10']/M['m00'])
-	cy = int(M['m01']/M['m00'])
-	color = img[cx,cy]
+	cx = int(M['m10'] / M['m00'])
+	cy = int(M['m01'] / M['m00'])
+	color = img[cy,cx]
+	text = '('+str(color[2])+','+str(color[1])+','+str(color[0])+')'
+	cv2.putText(img, text, (cx - 45, cy - 15),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 	print('The RGB Value is: (',color[2],',',color[1],',',color[0],')')
-	cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
+	cv2.drawContours(img, [c], -1, (0, 0, 0), 2)
 	cv2.imshow("Image", img)
 	cv2.waitKey(0)
 cv2.destroyAllWindows()
